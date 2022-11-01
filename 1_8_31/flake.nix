fa6bae8f3ee6368405732ee3f0fee0abfcfa5f66
hugosenari <hugosenari@gmail.com>
2022-11-01 10:52:24 -0300

    chore: re index bump

--- 1_8_31/flake.nix
+++ 1_8_31/flake.nix
@@ -7,19 +7,27 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-bump-master.flake = false;
-  inputs.src-bump-master.ref   = "refs/heads/master";
-  inputs.src-bump-master.owner = "disruptek";
-  inputs.src-bump-master.repo  = "bump";
-  inputs.src-bump-master.type  = "github";
+  inputs.src-bump-1_8_31.flake = false;
+  inputs.src-bump-1_8_31.ref   = "refs/tags/1.8.31";
+  inputs.src-bump-1_8_31.owner = "disruptek";
+  inputs.src-bump-1_8_31.repo  = "bump";
+  inputs.src-bump-1_8_31.type  = "github";
   
   inputs."github-disruptek-cutelog".owner = "nim-nix-pkgs";
   inputs."github-disruptek-cutelog".ref   = "master";
   inputs."github-disruptek-cutelog".repo  = "github-disruptek-cutelog";
-  inputs."github-disruptek-cutelog".dir   = "1_1_2";
+  inputs."github-disruptek-cutelog".dir   = "2_0_1";
   inputs."github-disruptek-cutelog".type  = "github";
   inputs."github-disruptek-cutelog".inputs.nixpkgs.follows = "nixpkgs";
   inputs."github-disruptek-cutelog".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."github.com/disruptek/cligen".owner = "nim-nix-pkgs";
+  inputs."github.com/disruptek/cligen".ref   = "master";
+  inputs."github.com/disruptek/cligen".repo  = "github.com/disruptek/cligen";
+  inputs."github.com/disruptek/cligen".dir   = "";
+  inputs."github.com/disruptek/cligen".type  = "github";
+  inputs."github.com/disruptek/cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."github.com/disruptek/cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   inputs."github-disruptek-balls".owner = "nim-nix-pkgs";
   inputs."github-disruptek-balls".ref   = "master";
@@ -29,24 +37,24 @@
   inputs."github-disruptek-balls".inputs.nixpkgs.follows = "nixpkgs";
   inputs."github-disruptek-balls".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
-  inputs."cligen".owner = "nim-nix-pkgs";
-  inputs."cligen".ref   = "master";
-  inputs."cligen".repo  = "cligen";
-  inputs."cligen".dir   = "v1_5_31";
-  inputs."cligen".type  = "github";
-  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs."github.com/disruptek/cligen".owner = "nim-nix-pkgs";
+  inputs."github.com/disruptek/cligen".ref   = "master";
+  inputs."github.com/disruptek/cligen".repo  = "github.com/disruptek/cligen";
+  inputs."github.com/disruptek/cligen".dir   = "";
+  inputs."github.com/disruptek/cligen".type  = "github";
+  inputs."github.com/disruptek/cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."github.com/disruptek/cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bump-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bump-1_8_31"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-bump-master";
+    src  = deps."src-bump-1_8_31";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
